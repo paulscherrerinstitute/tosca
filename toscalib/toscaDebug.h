f80bc399584acbb2115ac562b5a0b522461a942e
Dirk Zimoch <dirk.zimoch@psi.ch>
2016-08-31 10:28:50 +0200

    allow different debug levels

--- toscalib/toscaDebug.h
+++ toscalib/toscaDebug.h
@@ -8,7 +8,8 @@ int CAT(m,Debug); \
 FILE* CAT(m,DebugFile) = NULL;
 TOSCA_DEBUG_VARS(TOSCA_DEBUG_NAME)
 
-#define debug_internal(m, fmt, ...) if(CAT(m,Debug)) fprintf(CAT(m,DebugFile)?CAT(m,DebugFile):stderr, "%s: " fmt "\n", __FUNCTION__, ##__VA_ARGS__)
-#define debugErrno(fmt, ...) debug(fmt " failed: %m", ##__VA_ARGS__)
-#define debug(fmt, ...) debug_internal(TOSCA_DEBUG_NAME, fmt, ##__VA_ARGS__)
-#define error(fmt, ...) fprintf(stderr, "%s: " fmt "\n", __FUNCTION__, ##__VA_ARGS__)
+#define debug_internal(m,l,fmt,...) if(CAT(m,Debug)>=l) fprintf(CAT(m,DebugFile)?CAT(m,DebugFile):stderr, "%s: " fmt "\n", __FUNCTION__, ##__VA_ARGS__)
+#define debugErrno(fmt,...) debug(fmt" failed: %m",##__VA_ARGS__)
+#define debugLvl(l,fmt,...) debug_internal(TOSCA_DEBUG_NAME,l,fmt,##__VA_ARGS__)
+#define debug(fmt,...) debugLvl(1,fmt,##__VA_ARGS__)
+#define error(fmt,...) fprintf(stderr,"%s: "fmt"\n",__FUNCTION__,##__VA_ARGS__)
