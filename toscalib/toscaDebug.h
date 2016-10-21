dd25fc87861715bb08e9ed6761b63727bf6cd02a
Dirk Zimoch <dirk.zimoch@psi.ch>
2016-10-21 15:25:41 +0200

    print errors already at level 0

--- toscalib/toscaDebug.h
+++ toscalib/toscaDebug.h
@@ -17,7 +17,7 @@ TOSCA_DEBUG_VARS(TOSCA_DEBUG_NAME)
 #undef __TOSCA_DEBUG_VARS
 
 #define debug_internal(m,l,fmt,...) if(CAT(m,Debug)>=l) fprintf(CAT(m,DebugFile)?CAT(m,DebugFile):stderr, "%s: " fmt "\n", __FUNCTION__, ##__VA_ARGS__)
-#define debugErrno(fmt,...) debug(fmt" failed: %m",##__VA_ARGS__)
+#define debugErrno(fmt,...) debugLvl(-1, fmt" failed: %m",##__VA_ARGS__)
 #define debugLvl(l,fmt,...) debug_internal(TOSCA_DEBUG_NAME,l,fmt,##__VA_ARGS__)
 #define debug(fmt,...) debugLvl(1,fmt,##__VA_ARGS__)
 #define error(fmt,...) fprintf(stderr,"%s: "fmt"\n",__FUNCTION__,##__VA_ARGS__)
