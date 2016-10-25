ddb8ce41dc821f5c19bb06db213700b0ef3bbea7
Dirk Zimoch <dirk.zimoch@psi.ch>
2016-10-25 17:44:47 +0200

    add pev_evt_* functions

--- toscalib/toscaPev.c
+++ toscalib/toscaPev.c
@@ -1,71 +1,79 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
+#include <sys/select.h>
 
 #include <pevulib.h>
 #include <pevxulib.h>
 
 #include "toscaMap.h"
+#include "toscaIntr.h"
 #include "toscaElb.h"
 #include "i2c.h"
 
 #define TOSCA_DEBUG_NAME pev
 #include "toscaDebug.h"
 
-struct pev_node* pev_init(uint card)
-{
-    debug("pev compatibility mode");
-    return (void*) -1;
-}
+/** INIT **************************************************/
 
-struct pevx_node* pevx_init(uint card)
+static uint defaultCrate;
+
+struct pevx_node* pevx_init(uint crate)
 {
     debug("pevx compatibility mode");
     return (void*) -1;
 }
 
-/** CSR ****************************************************/
+struct pev_node* pev_init(uint crate)
+{
+    debug("pev compatibility mode");
+    defaultCrate = crate;
+    return (void*) -1;
+}
 
-int pevx_csr_rd(uint card, int address)
+/** CSR ***************************************************/
+
+int pevx_csr_rd(uint crate, int address)
 {
     if (address & 0x80000000)
-        return toscaCsrRead((card << 16) | (address & 0x7FFFFFFF));
+        return toscaCsrRead((crate << 16) | (address & 0x7FFFFFFF));
     else
-        return toscaIoRead((card << 16) | address);
+        return toscaIoRead((crate << 16) | address);
 }
 
 int pev_csr_rd(int address)
 {
-    return pevx_csr_rd(0, address);
+    return pevx_csr_rd(defaultCrate, address);
 }
 
-int pevx_csr_wr(uint card, int address, int value)
+int pevx_csr_wr(uint crate, int address, int value)
 {
     if (address & 0x80000000)
-        return toscaCsrWrite((card << 16) | (address & 0x7FFFFFFF), value);
+        return toscaCsrWrite((crate << 16) | (address & 0x7FFFFFFF), value);
     else
-        return toscaIoWrite((card << 16) | address, value);
+        return toscaIoWrite((crate << 16) | address, value);
 }
 
 void pev_csr_wr(int address, int value)
 {
-    pevx_csr_wr(0, address, value);
+    pevx_csr_wr(defaultCrate, address, value);
 }
 
-int pevx_csr_set(uint card, int address, int value)
+int pevx_csr_set(uint crate, int address, int value)
 {
     if (address & 0x80000000)
-        return toscaCsrSet((card << 16) | (address & 0x7FFFFFFF), value);
+        return toscaCsrSet((crate << 16) | (address & 0x7FFFFFFF), value);
     else
-        return toscaIoSet((card << 16) | address, value);
+        return toscaIoSet((crate << 16) | address, value);
 }
 
 void pev_csr_set(int address, int value)
 {
-    pevx_csr_set(0, address, value);
+    pevx_csr_set(defaultCrate, address, value);
 }
 
-/** ELB AND SRAM *******************************************/
+/** ELB AND SRAM ******************************************/
 
 static uint32_t *sramPtr(size_t address)
 {
@@ -83,7 +91,7 @@ static uint32_t *sramPtr(size_t address)
     return (uint32_t*) ((size_t) sram + address);
 }
 
-int pevx_elb_rd(uint card, int address)
+int pevx_elb_rd(uint crate, int address)
 {
     if (address >= 0xe000) /* sram */
     {
@@ -99,10 +107,10 @@ int pevx_elb_rd(uint card, int address)
 
 int pev_elb_rd(int address)
 {
-    return pevx_elb_rd(0, address);
+    return pevx_elb_rd(defaultCrate, address);
 }
 
-int pevx_elb_wr(uint card, int address, int value)
+int pevx_elb_wr(uint crate, int address, int value)
 {
     if (address >= 0xe000) /* sram */
     {
@@ -119,10 +127,10 @@ int pevx_elb_wr(uint card, int address, int value)
 
 int pev_elb_wr(int address, int value)
 {
-    return pevx_elb_wr(0, address, value);
+    return pevx_elb_wr(defaultCrate, address, value);
 }
 
-/** SMON ***************************************************/
+/** SMON **************************************************/
 
 int pev_smon_rd(int address)
 {
@@ -134,9 +142,9 @@ void pev_smon_wr(int address, int value)
     toscaSmonWrite(address, value);
 }
 
-/** BMR ****************************************************/
+/** BMR ***************************************************/
 
-static int pev_bmr_fd(unsigned int bmr, unsigned int address)
+static int pev_bmr_fd(uint bmr, uint address)
 {
     static int fd[4] = {0};
 
@@ -152,7 +160,7 @@ static int pev_bmr_fd(unsigned int bmr, unsigned int address)
     return fd[bmr];
 }
 
-int pev_bmr_read(unsigned int bmr, unsigned int address, unsigned int *value, unsigned int count)
+int pev_bmr_read(uint bmr, uint address, uint *value, uint count)
 {
     int fd;
     debug("bmr=%u address=0x%x, count=%u", bmr, address, count);
@@ -161,7 +169,7 @@ int pev_bmr_read(unsigned int bmr, unsigned int address, unsigned int *value, un
     return i2cRead(fd, address, count, value);
 }
 
-int pev_bmr_write(unsigned int bmr, unsigned int address, unsigned int value, unsigned int count)
+int pev_bmr_write(uint bmr, uint address, uint value, uint count)
 {
     int fd;
     debug("bmr=%u address=0x%x, value=0x%x  count=%u", bmr, address, value, count);
@@ -170,9 +178,9 @@ int pev_bmr_write(unsigned int bmr, unsigned int address, unsigned int value, un
     return i2cWrite(fd, address, count, value);
 }
 
-float pev_bmr_conv_11bit_u(unsigned short value)
+float pev_bmr_conv_11bit_u(ushort value)
 {
-    unsigned short l;
+    ushort l;
     short h;
 
     l = value & 0x7ff;
@@ -182,7 +190,7 @@ float pev_bmr_conv_11bit_u(unsigned short value)
     return(((float)l/(1 << h)));
 }
 
-float pev_bmr_conv_11bit_s(unsigned short value)
+float pev_bmr_conv_11bit_s(ushort value)
 {
     short h,l;
 
@@ -194,8 +202,295 @@ float pev_bmr_conv_11bit_s(unsigned short value)
     return(((float)l/(1 << h)));
 }
 
-float pev_bmr_conv_16bit_u(unsigned short value)
+float pev_bmr_conv_16bit_u(ushort value)
 {
     return(((float)value/(1 << 13)));
 }
 
+/** MAPS **************************************************/
+
+static int pev_mode_to_tosca_aspace(int mode)
+{
+    switch (mode & 0xff00)
+    {
+        case MAP_SPACE_VME|MAP_VME_CR:  return VME_CRCSR;
+        case MAP_SPACE_VME|MAP_VME_A16: return VME_A16;
+        case MAP_SPACE_VME|MAP_VME_A24: return VME_A24;
+        case MAP_SPACE_VME|MAP_VME_A32: return VME_A32;
+        case MAP_SPACE_SHM:             return TOSCA_SHM;
+        case MAP_SPACE_USR1:            return TOSCA_USER1;
+        case MAP_SPACE_USR2:            return TOSCA_USER2;
+        case MAP_SPACE_PCIE:            return VME_SLAVE;
+        default: return 0;
+    }
+}
+
+int pevx_map_alloc(uint crate, struct pev_ioctl_map_pg *map_p)
+{
+    volatile void* ptr;
+    toscaMapInfo_t mapInfo;
+    
+    map_p->offset = 0;
+    map_p->win_size = 0;
+    map_p->rem_base = 0;
+    map_p->loc_base = 0;
+    map_p->pci_base = 0;
+
+    if (map_p->sg_id == MAP_SLAVE_VME && (map_p->mode & 0xf000) != MAP_SPACE_PCIE)
+    {
+        debug("VME slave sg_id=0x%x mode=0x%x size=0x%x rem_addr=0x%lx loc_addr=0x%lx",
+            map_p->sg_id, map_p->mode, map_p->size, map_p->rem_addr, map_p->loc_addr);
+        toscaMapVMESlave(crate<<16 | pev_mode_to_tosca_aspace(map_p->mode),
+            map_p->loc_addr, map_p->size, map_p->rem_addr, (map_p->mode & MAP_SWAP_AUTO) != 0);
+        mapInfo = toscaCheckSlaveMaps(crate, map_p->rem_addr, map_p->size);
+        map_p->win_size = mapInfo.size;
+        map_p->rem_base = mapInfo.address;
+        map_p->loc_base = (size_t) mapInfo.ptr;
+        return 0;
+    }
+
+    debug("sg_id=0x%x mode=0x%x size=0x%x rem_addr=0x%lx",
+        map_p->sg_id, map_p->mode, map_p->size, map_p->rem_addr);
+
+    if (map_p->flag & MAP_FLAG_FORCE)
+    {
+        error("MAP_FLAG_FORCE not supported");
+        return -1;
+    }
+
+    map_p->loc_addr = 0;
+    ptr = toscaMap(crate<<16 | pev_mode_to_tosca_aspace(map_p->mode),
+        map_p->rem_addr, map_p->size);
+    if (!ptr) return -1;
+    mapInfo = toscaMapFind(ptr);
+    map_p->win_size = mapInfo.size;
+    map_p->rem_base = mapInfo.address;
+    map_p->loc_base = (size_t) mapInfo.ptr;
+    map_p->usr_addr = (void*) ptr;
+    return 0;
+}
+
+int pev_map_alloc(struct pev_ioctl_map_pg *map_p)
+{
+    return pevx_map_alloc(defaultCrate, map_p);
+}
+
+void *pevx_mmap(uint crate, struct pev_ioctl_map_pg *map_p)
+{
+    return map_p->usr_addr;
+}
+
+void *pev_mmap(struct pev_ioctl_map_pg *map_p)
+{
+    return map_p->usr_addr;
+}
+
+int pevx_munmap(uint crate, struct pev_ioctl_map_pg *map_p)
+{
+    /* do nothing, tosca will clean up at exit */
+    return 0;
+}
+
+int pev_munmap(struct pev_ioctl_map_pg *map_p)
+{
+    /* do nothing, tosca will clean up at exit */
+    return 0;
+}
+
+int pevx_map_free(uint crate, struct pev_ioctl_map_pg *map_p)
+{
+    /* do nothing, tosca will clean up at exit */
+    return 0;
+}
+
+int pev_map_free(struct pev_ioctl_map_pg *map_p)
+{
+    /* do nothing, tosca will clean up at exit */
+    return 0;
+}
+
+int pevx_map_modify(uint crate, struct pev_ioctl_map_pg *map_p)
+{
+    /* do nothing, cannot modify */
+    return -1;
+}
+
+int pev_map_modify(struct pev_ioctl_map_pg *map_p)
+{
+    /* do nothing, cannot modify */
+    return -1;
+}
+
+/** INTR **************************************************/
+
+typedef struct {
+    int fd[2];
+    uint32_t mask_vme;
+    uint32_t mask_usr;
+    int enabled;
+} my_pev_evt_queue;
+
+struct pev_ioctl_evt *pevx_evt_queue_alloc(uint crate, int sig)
+{
+    struct pev_ioctl_evt *evt;
+    evt = calloc(1, sizeof(struct pev_ioctl_evt) + sizeof(my_pev_evt_queue));
+    evt->sig = sig;
+    evt->evt_queue = evt + 1;
+    pipe(((my_pev_evt_queue*)evt->evt_queue)->fd);
+    return evt;
+}
+
+struct pev_ioctl_evt *pev_evt_queue_alloc(int sig)
+{
+    return pevx_evt_queue_alloc(defaultCrate, sig);
+}
+
+int pevx_evt_queue_free(uint crate, struct pev_ioctl_evt *evt)
+{
+    return 0;
+}
+
+int pev_evt_queue_free(struct pev_ioctl_evt *evt)
+{
+    return 0;
+}
+
+static void pev_intr_vme(struct pev_ioctl_evt *evt, int inum, int vec)
+{
+    my_pev_evt_queue *q = (my_pev_evt_queue*)evt->evt_queue;
+    if (!q->enabled) return;
+    if (q->mask_vme & (1 << inum)) return;
+    uint16_t ev = (EVT_SRC_VME | inum) << 8 | vec;
+    write(q->fd[1], &ev, 2);
+    if (evt->sig) kill(getpid(), evt->sig);
+}
+
+static void pev_intr_usr(struct pev_ioctl_evt *evt, int inum)
+{
+    my_pev_evt_queue *q = (my_pev_evt_queue*)evt->evt_queue;
+    if (!q->enabled) return;
+    if (q->mask_usr & (1 << inum)) return;
+    uint16_t ev = (EVT_SRC_USR1 | inum) << 8;
+    write(q->fd[1], &ev, 2);
+    if (evt->sig) kill(getpid(), evt->sig);
+}
+
+int pevx_evt_register(uint crate, struct pev_ioctl_evt *evt, int src_id)
+{
+    intrmask_t mask;
+    int vec;
+    switch (src_id & 0xf0)
+    {
+        case EVT_SRC_VME:
+            mask = TOSCA_VME_INTR(src_id & 0xf);
+            for (vec = 0; vec < 256; vec++)
+                toscaIntrConnectHandler(mask, vec, pev_intr_vme, evt);
+            break;
+        case EVT_SRC_USR1:
+        case EVT_SRC_USR2:
+            mask = TOSCA_USER1_INTR(src_id & 0x1f);
+            toscaIntrConnectHandler(mask, 0, pev_intr_usr, evt);
+            break;
+        default: return -1;
+    }
+    return 0;
+}
+
+int pev_evt_register(struct pev_ioctl_evt *evt, int src_id)
+{
+    return pevx_evt_register(defaultCrate, evt, src_id);
+}
+
+int pevx_evt_read(uint crate, struct pev_ioctl_evt *evt, int timeout)
+{
+    struct timeval tv;
+    fd_set read_fs;
+    int fd = ((my_pev_evt_queue*)evt->evt_queue)->fd[0];
+    int n;
+    uint16_t ev;
+
+    FD_ZERO(&read_fs);
+    FD_SET(fd, &read_fs);
+    if (timeout >= 0)
+    {
+        tv.tv_sec = timeout/1000;
+        tv.tv_usec = (timeout%1000)*1000;
+    }
+    n = select(fd + 1, &read_fs, NULL, NULL, timeout >= 0 ? &tv : NULL);
+    if (n < 1) return -1;
+    if (read(fd, &ev, 2) < 0) return -1;
+    return ev;
+}
+
+int pev_evt_read(struct pev_ioctl_evt *evt, int timeout)
+{
+    return pevx_evt_read(defaultCrate, evt, timeout);
+}
+
+int pevx_evt_queue_enable(uint crate, struct pev_ioctl_evt *evt)
+{
+    my_pev_evt_queue *q = (my_pev_evt_queue*)evt->evt_queue;
+    q->enabled = 1;
+    return 0;
+}
+
+int pev_evt_queue_enable(struct pev_ioctl_evt *evt)
+{
+    return pevx_evt_queue_enable(defaultCrate, evt);
+}
+
+int pevx_evt_queue_disable(uint crate, struct pev_ioctl_evt *evt)
+{
+    my_pev_evt_queue *q = (my_pev_evt_queue*)evt->evt_queue;
+    q->enabled = 0;
+    return 0;
+}
+
+int pev_evt_queue_disable(struct pev_ioctl_evt *evt)
+{
+    return pevx_evt_queue_disable(defaultCrate, evt);
+}
+
+int pevx_evt_mask(uint crate, struct pev_ioctl_evt *evt, int src_id)
+{
+    my_pev_evt_queue *q = (my_pev_evt_queue*)evt->evt_queue;
+    switch (src_id & 0xf0)
+    {
+        case EVT_SRC_VME:
+            q->mask_vme |= 1U << (src_id & 0xf);
+            break;
+        case EVT_SRC_USR1:
+        case EVT_SRC_USR2:
+            q->mask_usr |= 1U << (src_id & 0x1f);
+            break;
+        default: return -1;
+    }
+    return 0;
+}
+
+int pev_evt_mask(struct pev_ioctl_evt *evt, int src_id)
+{
+    return pevx_evt_mask(defaultCrate, evt, src_id);
+}
+
+int pevx_evt_unmask(uint crate, struct pev_ioctl_evt *evt, int src_id)
+{
+    my_pev_evt_queue *q = (my_pev_evt_queue*)evt->evt_queue;
+    switch (src_id & 0xf0)
+    {
+        case EVT_SRC_VME:
+            q->mask_vme &= ~(1U << (src_id & 0xf));
+            break;
+        case EVT_SRC_USR1:
+        case EVT_SRC_USR2:
+            q->mask_usr &= ~(1U << (src_id & 0x1f));
+            break;
+        default: return -1;
+    }
+    return 0;
+}
+
+int pev_evt_unmask(struct pev_ioctl_evt *evt, int src_id)
+{
+    return pevx_evt_unmask(defaultCrate, evt, src_id);
+}
