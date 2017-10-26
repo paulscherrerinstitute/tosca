cef5548841c77c2fdfa367fe767bcbc446ad65e5
Dirk Zimoch <dirk.zimoch@psi.ch>
2017-10-26 11:08:18 +0200

    new debuging utility added, toscaCopy renamed to memcopy

--- toscaUtils.c
+++ toscaUtils.c
@@ -56,6 +56,14 @@ void memfillSigAction(int sig __attribute__((unused)), siginfo_t *info, void *ct
 
 static void memfillFunc(const iocshArgBuf *args)
 {
+    toscaMapAddr_t addr;
+    uint32_t pattern;
+    size_t size;
+    int width;
+    int increment;
+    size_t i;
+    volatile void* address;
+
     struct sigaction sa = {
         .sa_sigaction = memfillSigAction,
         .sa_flags = SA_SIGINFO | SA_NODEFER, /* Do not block signal */
@@ -72,13 +80,11 @@ static void memfillFunc(const iocshArgBuf *args)
         iocshCmd("help memfill");
         return;
     }
-    toscaMapAddr_t addr = toscaStrToAddr(args[0].sval, NULL);
-    uint32_t pattern = args[1].ival;
-    size_t size = toscaStrToSize(args[2].sval);
-    int width = args[3].ival;
-    int increment = args[4].ival;
-    size_t i;
-    volatile void* address;
+    addr = toscaStrToAddr(args[0].sval, NULL);
+    pattern = args[1].ival;
+    size = toscaStrToSize(args[2].sval);
+    width = args[3].ival;
+    increment = args[4].ival;
     
     if (addr.addrspace)
         address = toscaMap(addr.addrspace, addr.address, size, 0);
@@ -123,25 +129,26 @@ static void memfillFunc(const iocshArgBuf *args)
     sigaction(SIGSEGV, &oldsa, NULL);
 }
 
-static const iocshFuncDef toscaCopyDef =
-    { "toscaCopy", 4, (const iocshArg *[]) {
+static const iocshFuncDef memcopyDef =
+    { "memcopy", 4, (const iocshArg *[]) {
     &(iocshArg) { "[addrspace:]source", iocshArgString },
     &(iocshArg) { "[addrspace:]dest", iocshArgString },
     &(iocshArg) { "size", iocshArgString },
     &(iocshArg) { "width", iocshArgInt },
 }};
 
-void toscaCopyFunc(const iocshArgBuf *args)
+void memcopyFunc(const iocshArgBuf *args)
 {
     struct timespec start, finished;
     volatile void* sourceptr;
     volatile void* destptr;
     toscaMapAddr_t addr;
     size_t size, i;
-    
+    int width;
+
     if (!args[0].sval || !args[1].sval || !args[2].sval)
     {
-        iocshCmd("help toscaCopy");
+        iocshCmd("help memcopy");
         return;
     }
     size = toscaStrToSize(args[2].sval);
@@ -167,7 +174,8 @@ void toscaCopyFunc(const iocshArgBuf *args)
         error("cannot map dest address %s", args[1].sval);
         return;
     }
-    int width = args[3].ival;
+
+    width = args[3].ival;
 
     if (toscaDmaDebug)
         clock_gettime(CLOCK_MONOTONIC_RAW, &start);
@@ -242,11 +250,115 @@ void toscaCopyFunc(const iocshArgBuf *args)
     
 }
 
+static const iocshFuncDef memcompDef =
+    { "memcomp", 4, (const iocshArg *[]) {
+    &(iocshArg) { "[addrspace:]source", iocshArgString },
+    &(iocshArg) { "[addrspace:]dest", iocshArgString },
+    &(iocshArg) { "size", iocshArgString },
+    &(iocshArg) { "width", iocshArgInt },
+}};
+
+static void memcompFunc(const iocshArgBuf *args)
+{
+    volatile uint32_t* sourceptr;
+    volatile uint32_t* destptr;
+    toscaMapAddr_t addr;
+    size_t size, i;
+    int width;
+
+    if (!args[0].sval || !args[1].sval || !args[2].sval)
+    {
+        iocshCmd("help memcomp");
+        return;
+    }
+    size = toscaStrToSize(args[2].sval);
+
+    addr = toscaStrToAddr(args[0].sval, NULL);
+    if (addr.addrspace)
+        sourceptr = toscaMap(addr.addrspace, addr.address, size, 0);
+    else
+        sourceptr = (volatile void*)(size_t)addr.address;
+    if (!sourceptr)
+    {
+        error("cannot map source address %s", args[0].sval);
+        return;
+    }
+
+    addr = toscaStrToAddr(args[1].sval, NULL);
+    if (addr.addrspace)
+        destptr = toscaMap(addr.addrspace, addr.address, size, 0);
+    else
+        destptr = (volatile void*)toscaStrToSize(args[1].sval);
+    if (!destptr)
+    {
+        error("cannot map dest address %s", args[1].sval);
+        return;
+    }
+
+    width = args[3].ival;
+
+    switch (width)
+    {
+        case 0:
+        case 1:
+        case -1:
+            for (i = 0; i < size; i++)
+            {
+                if (((uint8_t*)destptr)[i] != ((uint8_t*)sourceptr)[i]) break;
+            }
+            break;
+        case 2:
+            for (i = 0; i < size; i+=2)
+            {
+                if (((uint16_t*)destptr)[i/2] != ((uint16_t*)sourceptr)[i/2]) break;
+            }
+            break;
+        case 4:
+            for (i = 0; i < size; i+=4)
+            {
+                if (((uint32_t*)destptr)[i/4] != ((uint32_t*)sourceptr)[i/4]) break;
+            }
+            break;
+        case 8:
+            for (i = 0; i < size; i+=8)
+            {
+                if (((uint64_t*)destptr)[i/8] != ((uint64_t*)sourceptr)[i/8]) break;
+            }
+            break;
+        case -2:
+            for (i = 0; i < size; i+=2)
+            {
+                if (((uint16_t*)destptr)[i/2] != bswap_16(((uint16_t*)sourceptr)[i/2])) break;
+            }
+            break;
+        case -4:
+            for (i = 0; i < size; i+=4)
+            {
+                if (((uint32_t*)destptr)[i/4] != bswap_32(((uint32_t*)sourceptr)[i/4])) break;
+            }
+            break;
+        case -8:
+            for (i = 0; i < size; i+=8)
+            {
+                if (((uint64_t*)destptr)[i/8] != bswap_64(((uint64_t*)sourceptr)[i/8])) break;
+            }
+            break;
+        default:
+            error("Illegal width %d: must be 1, 2, 4, 8, -2, -4, -8", width);
+            return;
+    }
+    if (i < size)
+        printf("Mismatch at offset 0x%zx\n", i);
+    else
+        printf("OK\n");
+}
+
 static void toscaUtilsRegistrar(void)
 {
     iocshRegister(&mallocDef, mallocFunc);
     iocshRegister(&memfillDef, memfillFunc);
-    iocshRegister(&toscaCopyDef, toscaCopyFunc);
+    iocshRegister(&memcopyDef, memcopyFunc);
+    iocshRegister(&memcompDef, memcompFunc);
 }
 
 epicsExportRegistrar(toscaUtilsRegistrar);
